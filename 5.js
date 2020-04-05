2b0a7fdcd6ee06af5b1d0a1b816f2dcb026c7702
bagaspras97 <59312912+bagaspras97@users.noreply.github.com>
2020-04-05 12:21:33 +0700

    change format file 5.js

--- 5.js
+++ 5.js
@@ -1,71 +1,87 @@
-function hasil() {
-  var val = 1;
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <script>
+      function hasil() {
+        var val = 1;
 
-  var arr = [];
-  for (val; val < 5000; val++) {
-    val = val.toString();
-    var length = val.length;
-    if (length == 1) {
-      cek1();
-    } else if (length == 2) {
-      cek2();
-    } else if (length == 3) {
-      cek3();
-    } else {
-      cek4();
-    }
-  }
+        var arr = [];
+        for (val; val < 5000; val++) {
+          val = val.toString();
+          var length = val.length;
+          if (length == 1) {
+            cek1();
+          } else if (length == 2) {
+            cek2();
+          } else if (length == 3) {
+            cek3();
+          } else {
+            cek4();
+          }
+        }
 
-  function cek1() {
-    var newVal = parseInt(val) + parseInt(val);
-    arr.push(newVal);
-    return arr;
-  }
+        function cek1() {
+          var newVal = parseInt(val) + parseInt(val);
+          arr.push(newVal);
+          return arr;
+        }
 
-  function cek2() {
-    var newVal = parseInt(val) + parseInt(val[0]) + parseInt(val[1]);
-    arr.push(newVal);
-    return arr;
-  }
+        function cek2() {
+          var newVal = parseInt(val) + parseInt(val[0]) + parseInt(val[1]);
+          arr.push(newVal);
+          return arr;
+        }
 
-  function cek3() {
-    var newVal =
-      parseInt(val) + parseInt(val[0]) + parseInt(val[1]) + parseInt(val[2]);
-    arr.push(newVal);
-    return arr;
-  }
+        function cek3() {
+          var newVal =
+            parseInt(val) +
+            parseInt(val[0]) +
+            parseInt(val[1]) +
+            parseInt(val[2]);
+          arr.push(newVal);
+          return arr;
+        }
 
-  function cek4() {
-    var newVal =
-      parseInt(val) +
-      parseInt(val[0]) +
-      parseInt(val[1]) +
-      parseInt(val[2]) +
-      parseInt(val[3]);
-    arr.push(newVal);
-    return val;
-  }
-  return arr;
-}
+        function cek4() {
+          var newVal =
+            parseInt(val) +
+            parseInt(val[0]) +
+            parseInt(val[1]) +
+            parseInt(val[2]) +
+            parseInt(val[3]);
+          arr.push(newVal);
+          return val;
+        }
+        return arr;
+      }
 
-//console.log(
-//  hasil().sort((a, b) => {
-//    return a - b;
-//  })
-//);
+      //console.log(
+      //  hasil().sort((a, b) => {
+      //    return a - b;
+      //  })
+      //);
 
-var newArr = [];
-var final = 0;
-//   console.log(hasil().includes(2));
-for (i = 1; i < hasil().length; i++) {
-  if (hasil().includes(i)) {
-  } else {
-    newArr.push(i);
-  }
-}
-//   console.log(newArr);
+      var newArr = [];
+      var final = 0;
+      //   console.log(hasil().includes(2));
+      for (i = 1; i < hasil().length; i++) {
+        if (hasil().includes(i)) {
+        } else {
+          newArr.push(i);
+        }
+      }
+      //   console.log(newArr);
 
-for (i = 0; i < newArr.length; i++) {
-  final += newArr[i];
-}
-console.log(final);
+      for (i = 0; i < newArr.length; i++) {
+        final += newArr[i];
+      }
+      console.log(final);
+    </script>
+  </body>
+</html>
+
